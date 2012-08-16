298ba56520c9ebc8337fe92c488a57f650aa715a
Nate Hardison <natehardison@gmail.com>
2012-08-16 14:52:32 -0400

    Finally deleting these old files

--- rewriter.h
+++
@@ -1,9 +0,0 @@
-#ifndef CLANG_REWRITER_REWRITER_H
-#define CLANG_REWRITER_REWRITER_H
-
-#include <string>
-#include <set>
-
-void rewrite(std::string filename, std::set<std::string> &functions);
-
-#endif /* CLANG_REWRITER_REWRITER_H */
